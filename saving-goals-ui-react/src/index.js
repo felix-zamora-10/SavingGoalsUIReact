b54b99a689a659a0ac0b44369d8f06a07703543d
Felix Z <49648575+felix-zamora-10@users.noreply.github.com>
2020-04-07 16:59:06 -0600

    Se hacen pruebas en el proyecto y se eliminan archivos que no se usarán.

--- saving-goals-ui-react/src/index.js
+++ saving-goals-ui-react/src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const user = {
+  firstName: "Félix",
+  lastName: "Zamora",
+  avatar:
+    "http://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png",
+};
+
+function getName(user) {
+  return `${user.firstName} ${user.lastName}`;
+}
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hola {getName(user)}.</h1>;
+  }
+
+  return <h1>Hola extraño.</h1>;
+}
+
+// const name = "Félix";
+// const element = <h1>Hola {getName(user)}.</h1>;
+// const element = <div>{getGreeting(user)}</div>;
+// const element = <img src={user.avatar} />;
+const element = (
+  <div>
+    <h1>Hola {getGreeting(user)}</h1>
+    <img src={user.avatar} />
+  </div>
 );
+const container = document.getElementById("root");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ReactDom.render(_Que_, _Donde_);
+ReactDom.render(element, container);
+
+// const element = document.createElement("h1");
+// const container = document.getElementById("root");
+// element.innerText = "Hola mundo";
+// container.appendChild(element);
